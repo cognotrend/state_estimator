31f335e12b8450e8b4d5aec4d6d6352c2ed1f751
musicarroll <musicarroll@gmail.com>
2022-01-26 19:49:01 -0600

    Added argparse processing
    Now processes three optional CLIs.

--- hier2.py
+++ hier2.py
@@ -3,19 +3,44 @@ import stockmeas as sm
 import kalplots as kp
 import numpy as np
 import json
-
-# This filter is not a composite, but instead builds a weighted average of each filter's output
-# This would be a good opportunity to use class variables and static functions.  The compute the averages
-# after each cycle.  But that will be a later enhancement.
+import argparse
+# This filter is not a composite, but instead builds a weighted average of each 
+# filter's output.
+# This would be a good opportunity to use class variables and static functions.  
+# The compute the averages after each cycle.  But that will be a later enhancement.
+# TODO:
+# Improve argument handling with argparse
 # IBM stats:
 # mean:	0.00004333	-0.00000029
 # variance:	0.00027046	0.00056898
 # exp(mean)	1.00004333	0.99999971
 # exp(variance)	1.00027050	1.00056914
-mynumruns =100
+parser = argparse.ArgumentParser()
+parser.add_argument('--numruns','-nr', help='Number of kalman cycles to execute.')
+parser.add_argument('--stocks','-s', help='Comma-separated list of stocks.')
+parser.add_argument('--lmode','-lm',help='Run in log mode if present.')
+args = parser.parse_args()
+if args.numruns is not None:
+    mynumruns = int(args.numruns)
+else:
+    mynumruns = 200
+
+if args.numruns is not None:
+    stock_str = args.stocks
+    if stock_str.find(',')>-1:
+        stocks = stock_str.split(',')
+    else:
+        stocks = [stock_str]
 # stocks = ['IBM','AMZN','GE','MSFT','IBM','AMZN','GE','MSFT','IBM','AMZN','GE','MSFT','IBM','AMZN','GE','MSFT']
 # q_factors = [.1,1.0,10.0,100.0,.2,.2,.2,.2,.3,.3,.3,.3,.4,.4,.4,.4]
-stocks = ['IBM','GE','MSFT']
+else:
+    stocks = ['IBM','GE','MSFT']
+
+if args.lmode is not None:
+    logmode = 1
+else:
+    logmode = 0
+
 q_factors = [1.0,1.0,1.0]
 mykfs = []
 i=0
@@ -24,7 +49,7 @@ for stock in stocks:
     title_prefix='Std 3-state filter: '+stock
     my_legend_str = [stock]
     mysm_tmp = sm.StockMeasurement(noiseSigma=10, # added measurement noise
-                            logmode=1,
+                            logmode=logmode,
                             infiles=infiles
                             )
     q_factor = q_factors[i]
@@ -37,7 +62,7 @@ for stock in stocks:
                         phi_type=1,
                         sigma=5.0,  # BTC prices are accurately reported
                         num_runs=mynumruns,
-                        logmode=1, 
+                        logmode=logmode, 
                         num_blocks=1,
                         displayflag=False,
                         verbose=False,
@@ -122,9 +147,12 @@ for i in range(mynumruns):
     mykf.x_plus[:,i] = weighted_sum.reshape((3,))
     mykf.x_minus[:,i] = weighted_sum_minus.reshape((3,))
     tmp = mykf.x_minus[:,i].reshape((3,1))
-    tmpz = mykf.z[:,:,i].reshape((3,1))
-    mykf.H[1,0]=1.0
-    mykf.H[2,0]=1.0
+    if mykf.z[:,:,i].shape != (1,1):
+        tmpz = mykf.z[:,:,i].reshape((3,1))
+        mykf.H[1,0]=1.0
+        mykf.H[2,0]=1.0
+    else:
+        tmpz = mykf.z[:,:,i]
     tmp_zhat = np.dot(mykf.H,tmp)
     mykf.residual[:,:,i] = tmpz - tmp_zhat
 
